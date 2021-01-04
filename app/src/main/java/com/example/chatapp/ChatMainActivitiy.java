451456a17f444729dfd4c2c1b5f93774ef4037ca
jimmey <jimmeyjib@gmail.com>
2021-01-04 19:49:06 +0900

    initial Commit

--- app/src/main/java/com/example/chatapp/ChatMainActivitiy.java
+++ app/src/main/java/com/example/chatapp/ChatMainActivitiy.java
@@ -1,23 +1,27 @@
 package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
 public class ChatMainActivitiy extends AppCompatActivity {
 
-    private Toolbar toolbar;
+    static final String[] LIST_MENU = {"LIST1", "LIST2", "LIST3"} ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_main_activitiy);
 
-        /*Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.app_name);
-        setSupportActionBar(toolbar);*/
+        RecyclerView recyclerView = findViewById(R.id.RecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+      //  recyclerView.setAdapter(new RecyclerAdapter());
     }
 }
